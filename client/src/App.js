11efb613415fca046b43b7ad15a535f9f4525e7b
Mitchell George <mitchgeorge8@gmail.com>
2022-09-14 00:20:46 -0400

    Created Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile"
-import Signup from "./pages/Signup"
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
